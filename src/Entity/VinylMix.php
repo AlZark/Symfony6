fc0599e81c59e068ff19f0f1a0bf112744fb4d7b
Algirdas <alzarkaitis22@gmail.com>
2023-01-17 08:42:54 +0200

    Trying to make doctrine work

---
+++ src/Entity/VinylMix.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\VinylMixRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: VinylMixRepository::class)]
+class VinylMix
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $title = null;
+
+    #[ORM\Column(type: Types::TEXT, nullable: true)]
+    private ?string $descritpion = null;
+
+    #[ORM\Column]
+    private ?int $trackCount = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $genre = null;
+
+    #[ORM\Column]
+    private ?\DateTimeImmutable $createdAd = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): self
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getDescritpion(): ?string
+    {
+        return $this->descritpion;
+    }
+
+    public function setDescritpion(?string $descritpion): self
+    {
+        $this->descritpion = $descritpion;
+
+        return $this;
+    }
+
+    public function getTrackCount(): ?int
+    {
+        return $this->trackCount;
+    }
+
+    public function setTrackCount(int $trackCount): self
+    {
+        $this->trackCount = $trackCount;
+
+        return $this;
+    }
+
+    public function getGenre(): ?string
+    {
+        return $this->genre;
+    }
+
+    public function setGenre(string $genre): self
+    {
+        $this->genre = $genre;
+
+        return $this;
+    }
+
+    public function getCreatedAd(): ?\DateTimeImmutable
+    {
+        return $this->createdAd;
+    }
+
+    public function setCreatedAd(\DateTimeImmutable $createdAd): self
+    {
+        $this->createdAd = $createdAd;
+
+        return $this;
+    }
+}
